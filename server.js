557962fca7c240fd75c695cc62e1022f0b87200f
Edwin Chajon <junchajo@gmail.com>
2020-02-17 20:36:32 -0800

    queries

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const db = require('./data/dbConfig.js');
 
 const server = express();
 
+const router = require('./Router/router');
+
 server.use(express.json());
+
+server.use('/accounts', router);
 
 module.exports = server;
